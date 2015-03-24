8e03ef9382aa114c338e0dd1cb32ae4354ec60d2
tamilrahman <tamilrahmanic@gmail.com>
2015-03-24 16:10:37 +0530

    devise librarian

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324103033) do
+ActiveRecord::Schema.define(version: 20150324103815) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,23 @@ ActiveRecord::Schema.define(version: 20150324103033) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "librarians", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "librarians", ["email"], name: "index_librarians_on_email", unique: true
+  add_index "librarians", ["reset_password_token"], name: "index_librarians_on_reset_password_token", unique: true
 
 end
