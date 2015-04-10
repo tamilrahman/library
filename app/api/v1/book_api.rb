f3a53968def887a909ac7d1a2753d3cab6d532a3
tamilrahman <tamilrahmanic@gmail.com>
2015-04-10 14:47:59 +0530

    extend date

--- app/api/v1/book_api.rb
+++ app/api/v1/book_api.rb
@@ -8,5 +8,19 @@ module V1
 			get 'mybooks', rabl: 'mybooks.rabl' do
 				@mybooks = @current_user.books
 			end
+
+			params do
+				requires :book_id, type: Integer
+			end
+			post 'extend_date' do
+				@book = Book.find(params[:book_id])
+				@user_book = UserBook.find_by(book_id: @book.id, user_id: @current_user.id) 
+				Rails.logger.info "------------------#{@user_book.inspect}"
+				if @user_book.update(date: @user_book.date + 10)
+					success_response("Book date is extended")
+				else
+					failure_response("Cann't be done")
+				end
+			end
 	end	
 end
--- app/views/api/mybooks.rabl
+++ app/views/api/mybooks.rabl
@@ -1,7 +1,7 @@
 object false
 child @mybooks, object_root: false do
-	attributes :name, :isbn_no
-	node(:date){ |u| u.created_at.strftime("%b %d, %Y")}
-	node(:author){ |u| u.authors.name}
-	node(:category) {"Engineering books"}
+	attributes :id, :name, :isbn_no
+	node(:date){ |u| u.user_books.first.date.strftime("%b %d, %Y")}
+	node(:author){ |u| u.authors.first.name}
+	node(:category) {|u| u.categories.first.category_name}
 end
